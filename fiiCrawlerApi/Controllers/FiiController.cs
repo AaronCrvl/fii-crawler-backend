054a45db051f7e3e6cb16c185f484d6e2e883c98
Aaron <72924198+AaronCrvl@users.noreply.github.com>
2023-09-20 22:29:08 -0300

    📟fii model and controller

---
+++ fiiCrawlerApi/Controllers/FiiController.cs
@@ -0,0 +1,41 @@
+﻿using fiiCrawlerApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace fiiCrawlerApi.Controllers
+{
+    public class FiiController : Controller
+    {
+        [HttpGet]
+        //[EnableCors()]
+        [Route("getListaResumo/")]
+        //https://localhost:44393/Fii/getListaResumo/
+        public ActionResult getListaResumo()
+        {
+            try
+            {
+
+                var cache = new fiiCrawlerApi.Cache.GerenciadorDeCache();
+                var crawler = new fiiCrawlerApi.WebScraper.Crawler();
+                List<Fii> dados = crawler.GetListaResumoFii().Result;
+                cache.SalvarCache(dados);
+
+                
+                HttpContext.Response.StatusCode = (int)HttpStatusCode.OK;
+                var res400 = new ContentResult();
+                res400.Content = "Something went wrong.";
+                res400.ContentType = "application/json";
+                //res400.ContentEncoding = System.Text.Encoding.UTF8;
+                return res400;
+            }
+            catch (Exception ex)
+            {
+                return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
---
+++ fiiCrawlerApi/Models/Fii.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fiiCrawlerApi.Models
+{
+    public class Fii
+    {
+        public string fii;
+        public string ultimoRedimentoRS;
+        public string ultimosRedimento;
+        public string dataPagamento;
+        public string dataBase;
+        public string rendimentoMedioAnual;
+        public string patrimonio;
+        public string cota;
+    }
+}
